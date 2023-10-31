fa5878f6f16571cdf812db24080c1a2f4b585446
Kyere <rnstkr@gmail.com>
2023-10-31 15:50:48 -0700

    Python - if/else, loops, functions updated2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 num = 0
 while num <= 89:
     if num % 10 == 0:
-        num += 1 + number // 10
+        num += 1 + num // 10
     print("{:02d}".format(num), end='\n' if num == 89 else ", ")
     num += 1
